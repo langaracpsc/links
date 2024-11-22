22db9fd8edef9f69bdb894bca8d6561617e5faf1
Anderson T <tseng.andersonn@gmail.com>
2024-11-21 18:45:09 -0800

    add new shortlink

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ referral_code = "linkspage"
 links = {
     "register": f"https://lu.ma/g2q0djtw?utm_source={referral_code}",
     "hiring": f"https://forms.gle/hdojrJcQXKLxU14W7",
+    "build" : f"https://hack-nights-lcs.devpost.com",
     # Add more links here as needed
 }
 
